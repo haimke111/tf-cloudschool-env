d2e11544cbaf0fdfd4aa4a1814d57594be9d14b5
Haim Steinberg <haimke8200@gmail.com>
2019-09-25 16:00:09 +0300

    fixinig the db provision script

--- modules/db_provision.sql
+++ modules/db_provision.sql
@@ -1,1 +1,13 @@
-create database ClsFinalProject;
+create database if not exists ClsFinalProject;
+-- Create a new table called 'TableName' in schema 'SchemaName'
+-- Drop the table if it already exists
+-- IF OBJECT_ID('ClsFinalProject.APIData', 'U') IS NOT NULL
+DROP TABLE IF EXISTS ClsFinalProject.APIData;
+
+-- Create the table in the specified schema
+CREATE TABLE ClsFinalProject.APIData
+(
+    EndPointName CHAR(10) NOT NULL,
+    EndPointId INT NOT NULL,
+    EndPointData JSON NOT NULL
+);
