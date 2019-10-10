4314040974d6af932778d61a7e750194fd5c21ec
Haim Steinberg <haimke8200@gmail.com>
2019-10-10 08:48:32 +0300

    changing ELB port for testing, changing user data script to install python3

--- main.tf
+++ main.tf
@@ -118,7 +118,7 @@ resource "aws_elb" "project-app-elb" {
   listener {
     instance_port     = 80
     instance_protocol = "http"
-    lb_port           = 8080
+    lb_port           = 80
     lb_protocol       = "http"
   }
 
@@ -126,7 +126,7 @@ resource "aws_elb" "project-app-elb" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "HTTP:8080/"
+    target              = "HTTP:80/"
     interval            = 30
   }
 
